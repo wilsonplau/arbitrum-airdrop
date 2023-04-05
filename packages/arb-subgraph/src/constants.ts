c1625aed0e8b897cfc18b5c170f452f924e35cab
Wilson Lau <hello@wilsonplau.com>
2023-04-05 11:03:29 +0900

    feat: add more statistics + separate helpers

---
+++ packages/arb-subgraph/src/constants.ts
@@ -0,0 +1,4 @@
+import { BigInt } from "@graphprotocol/graph-ts";
+
+export let TOTAL_ID = "total";
+export let INCREMENT = BigInt.fromI32(1);
--- packages/arb-subgraph/src/l-2-arbitrum-token.ts
+++ packages/arb-subgraph/src/l-2-arbitrum-token.ts
@@ -1,55 +1,14 @@
-import { BigInt, Bytes } from "@graphprotocol/graph-ts";
 import {
   Transfer,
   DelegateVotesChanged,
 } from "../generated/L2ArbitrumToken/L2ArbitrumToken";
+import { TransferEvent, DelegateVotesChangedEvent } from "../generated/schema";
 import {
-  TransferEvent,
-  TokenBalance,
-  DailyTransferStat,
-  DelegateVotesChangedEvent,
-  DelegateVotesBalance,
-} from "../generated/schema";
-
-let INCREMENT = BigInt.fromI32(1);
-
-function _timestampToDay(timestamp: BigInt): BigInt {
-  return timestamp.div(BigInt.fromI32(86400)).times(BigInt.fromI32(86400));
-}
-
-function _loadOrCreateTokenBalanceEntity(address: Bytes): TokenBalance {
-  let tokenBalanceEntity = TokenBalance.load(address);
-  if (tokenBalanceEntity == null) {
-    tokenBalanceEntity = new TokenBalance(address);
-    tokenBalanceEntity.address = address;
-    tokenBalanceEntity.balance = BigInt.fromI32(0);
-  }
-  return tokenBalanceEntity;
-}
-
-function _loadOrCreateDailyTransferStat(timestamp: BigInt): DailyTransferStat {
-  let day = _timestampToDay(timestamp);
-  let dailyTransferStatEntity = DailyTransferStat.load(day.toString());
-  if (dailyTransferStatEntity == null) {
-    dailyTransferStatEntity = new DailyTransferStat(day.toString());
-    dailyTransferStatEntity.timestamp = day.toI32();
-    dailyTransferStatEntity.totalTransfers = BigInt.fromI32(0);
-    dailyTransferStatEntity.totalTransfersAmount = BigInt.fromI32(0);
-  }
-  return dailyTransferStatEntity;
-}
-
-function _loadOrCreateDelegateVotesBalanceEntity(
-  address: Bytes
-): DelegateVotesBalance {
-  let delegateVotesBalanceEntity = DelegateVotesBalance.load(address);
-  if (delegateVotesBalanceEntity == null) {
-    delegateVotesBalanceEntity = new DelegateVotesBalance(address);
-    delegateVotesBalanceEntity.address = address;
-    delegateVotesBalanceEntity.balance = BigInt.fromI32(0);
-  }
-  return delegateVotesBalanceEntity;
-}
+  _loadOrCreateDailyTransferStat,
+  _loadOrCreateDelegateVotesBalance,
+  _loadOrCreateTokenBalance,
+} from "./helpers";
+import { INCREMENT } from "./constants";
 
 export function handleTransfer(event: Transfer): void {
   let eventEntity = new TransferEvent(
@@ -63,27 +22,20 @@ export function handleTransfer(event: Transfer): void {
   eventEntity.transactionHash = event.transaction.hash;
   eventEntity.save();
 
-  let tokenBalanceEntityFrom = _loadOrCreateTokenBalanceEntity(
-    event.params.from
-  );
-  tokenBalanceEntityFrom.balance = tokenBalanceEntityFrom.balance.minus(
-    event.params.value
-  );
-  tokenBalanceEntityFrom.save();
+  let tokenBalanceFrom = _loadOrCreateTokenBalance(event.params.from);
+  tokenBalanceFrom.balance = tokenBalanceFrom.balance.minus(event.params.value);
+  tokenBalanceFrom.save();
 
-  let tokenBalanceEntityTo = _loadOrCreateTokenBalanceEntity(event.params.to);
-  tokenBalanceEntityTo.balance = tokenBalanceEntityTo.balance.plus(
-    event.params.value
-  );
-  tokenBalanceEntityTo.save();
+  let tokenBalanceTo = _loadOrCreateTokenBalance(event.params.to);
+  tokenBalanceTo.balance = tokenBalanceTo.balance.plus(event.params.value);
+  tokenBalanceTo.save();
 
-  let day = _timestampToDay(event.block.timestamp);
-  let dailyTransferStatEntity = _loadOrCreateDailyTransferStat(day);
-  dailyTransferStatEntity.totalTransfers =
-    dailyTransferStatEntity.totalTransfers.plus(INCREMENT);
-  dailyTransferStatEntity.totalTransfersAmount =
-    dailyTransferStatEntity.totalTransfersAmount.plus(event.params.value);
-  dailyTransferStatEntity.save();
+  let dailyTransferStat = _loadOrCreateDailyTransferStat(event.block.timestamp);
+  dailyTransferStat.totalTransfers =
+    dailyTransferStat.totalTransfers.plus(INCREMENT);
+  dailyTransferStat.totalTransfersAmount =
+    dailyTransferStat.totalTransfersAmount.plus(event.params.value);
+  dailyTransferStat.save();
 }
 
 export function handleDelegateVotesChanged(event: DelegateVotesChanged): void {
@@ -98,9 +50,9 @@ export function handleDelegateVotesChanged(event: DelegateVotesChanged): void {
   eventEntity.transactionHash = event.transaction.hash;
   eventEntity.save();
 
-  let delegateVotesBalanceEntity = _loadOrCreateDelegateVotesBalanceEntity(
+  let delegateVotesBalance = _loadOrCreateDelegateVotesBalance(
     event.params.delegate
   );
-  delegateVotesBalanceEntity.balance = event.params.newBalance;
-  delegateVotesBalanceEntity.save();
+  delegateVotesBalance.balance = event.params.newBalance;
+  delegateVotesBalance.save();
 }
